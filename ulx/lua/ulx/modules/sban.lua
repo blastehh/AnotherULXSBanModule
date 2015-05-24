a6339a54d63b89a4f5eea1aefe0f12665540f101
Matthew Chan <blastuk@gmail.com>
2015-05-24 05:15:35 +0100

    Updated for ulx 3.7

--- ulx/lua/ulx/modules/sban.lua
+++ ulx/lua/ulx/modules/sban.lua
@@ -1,5 +1,5 @@
 --[[
---		Another ULX Source Bans Module 0.7
+--		Another ULX Source Bans Module 0.8
 --
 --		CREDITS:
 --		This sban module was based on a very old version of ULX Source Bans Module by FunDK http://facepunch.com/showthread.php?t=1311847
